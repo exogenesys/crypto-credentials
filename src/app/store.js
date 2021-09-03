c1732db25ce12d26e0955ef0a2a9284685c41e4d
Harsh Gautam <hrshgtm9@gmail.com>
2021-09-03 15:05:18 +0530

    added css + fontawesome

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
-import thunkMiddleware from 'redux-thunk'
-import AuthReducer from './auth/authReducer'
-import TransactionReducer from "./transaction/transactionReducer"
-
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import thunkMiddleware from "redux-thunk";
+import AuthReducer from "./auth/authReducer";
+import TransactionReducer from "./transaction/transactionReducer";
 
 export default configureStore({
-    reducer: combineReducers({auth: AuthReducer, transaction: TransactionReducer}),
-    middleware: [thunkMiddleware]
-})
-
+  reducer: combineReducers({
+    auth: AuthReducer,
+    transaction: TransactionReducer,
+  }),
+  middleware: [thunkMiddleware],
+});
